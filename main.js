b2c9a7394072cc450fbca3bf3b6140dfc1e33870
dsanchezlo <44549545+dsanchezlo@users.noreply.github.com>
2018-11-23 20:21:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Desde aquí se inicializan todos los estados del mapa, y esto se hace gracias a phaser.
+
 var mapa = new Phaser.Game(513, 500, Phaser.AUTO, "contenedor-mapa");
 
 mapa.state.add ( "MapaUno", mapaUno );
